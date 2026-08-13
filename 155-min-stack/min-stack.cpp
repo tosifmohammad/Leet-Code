5a8452e826ec61c761decf6a2b1fa2eaa5c34894
tosifmohammad <tosifkhan8122@gmail.com>
2026-08-13 23:27:04 +0530

    Time: 79 ms (8.94%) | Memory: 151.1 MB (79.14%) - LeetSync

---
+++ 155-min-stack/min-stack.cpp
@@ -0,0 +1,38 @@
+class MinStack {
+public:
+    stack<pair<int,int>> s;
+    MinStack() {
+        
+    }
+    
+    void push(int value) {
+        if(s.empty()){
+            s.push( {value, value});
+        }
+        else{
+            int minVal = min(value,s.top().second);
+            s.push({value,minVal});
+        }
+    }
+    
+    void pop() {
+        s.pop();
+    }
+    
+    int top() {
+        return s.top().first;
+    }
+    
+    int getMin() {
+        return s.top().second;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(value);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
