3c9cc91aacacd82de536fdf0364011992a92b730
tosifmohammad <tosifkhan8122@gmail.com>
2026-09-24 21:05:44 +0530

    Time: 0 ms (100.00%) | Memory: 34.4 MB (50.89%) - LeetSync

---
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root == NULL){
+            return root;
+        }
+        if(key < root->val){
+            root->left = deleteNode(root->left , key);
+        }
+        else if(key > root->val){
+            root->right = deleteNode(root->right, key);
+        }
+        else{
+            if(root->left == NULL && root->right == NULL){
+                return NULL;
+            }
+            if(root->left == NULL){
+                return root->right;
+            }
+            else if(root->right == NULL){
+                return root->left;
+            }else{
+                TreeNode* node = findPrededecesor(root->left);
+                root->val = node->val;
+                root->left = deleteNode(root->left ,node->val);
+
+            }
+        }
+        return root;
+    
+    }
+    TreeNode* findPrededecesor(TreeNode* node){
+        while(node->right != NULL){
+            node = node->right;
+        }
+        return node;
+    }
+};
